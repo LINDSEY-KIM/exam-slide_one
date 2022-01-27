e3109d958f0b62e37f1e9f8fc71dd1bb36be3bf3
LINDSEY-KIM <88068327+LINDSEY-KIM@users.noreply.github.com>
2022-01-28 00:09:41 +0900

    add js code

--- js/main.js
+++ js/main.js
@@ -2,14 +2,37 @@
 let i = 0;
 let j = 4;
 
+const dots = document.querySelectorAll(".dot_container button");
+const images = document.querySelectorAll(".image_container img")
+
 function next(){
     document.getElementById("content" + (i+1)).classList.remove("active");
     i = ( j + i + 1 ) % j;
     document.getElementById("content" + (i+1)).classList.add("active");
+
+    indicator(i+1);
 }
 
 function prev(){
     document.getElementById("content" + (i+1)).classList.remove("active");
     i = ( j + i - 1 ) % j;
     document.getElementById("content" + (i+1)).classList.add("active");
+
+    indicator(i+1);
+}
+
+function indicator(num){
+    dots.forEach(function(dot){
+        dot.style.backgroundColor = "transparent";
+    })
+    document.querySelector(".dot_container button:nth-child(" + num +  ")").style.backgroundColor = "#7f53ac";
+}
+
+function dot(index){
+    images.forEach(function(image){
+        image.classList.remove("active");
+    });
+    document.getElementById("content" + index).classList.add("active");
+    i = index - 1;
+    indicator(index);
 }
